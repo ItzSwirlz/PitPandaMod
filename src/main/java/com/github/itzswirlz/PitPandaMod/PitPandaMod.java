5e20ba81a195d46318e9ebcecf42d5676d7a5f8d
Joshua Peisach <itzswirlz2020@outlook.com>
2021-09-07 21:50:33 -0400

    PitPandaMod.java: preInit() is a FMLPreInitializationEvent, not a normal

--- src/main/java/com/github/itzswirlz/PitPandaMod/PitPandaMod.java
+++ src/main/java/com/github/itzswirlz/PitPandaMod/PitPandaMod.java
@@ -5,16 +5,16 @@ import com.github.itzswirlz.PitPandaMod.commands.CheckPitScammer;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = PitPandaMod.MODID, version = PitPandaMod.VERSION)
 public class PitPandaMod
 {
     public static final String MODID = "pitpandamod";
     public static final String VERSION = "0.0.1";
-    
+
     @EventHandler
-    public void preInit(FMLInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event) {
     	ClientCommandHandler.instance.registerCommand(new CheckPitScammer());
     }
 
